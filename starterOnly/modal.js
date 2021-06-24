31247ce88b1d9e3d24ac4519f158b4dcd0a53b08
picyoh <picotyoh@gmail.com>
2021-06-24 12:06:47 +0200

    fermeture modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,6 +19,15 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+//close event
+
+closeBtn.addEventListener("click", closeModal);
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
 }
 
 
